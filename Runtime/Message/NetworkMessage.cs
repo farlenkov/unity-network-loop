bae7dd993f91d4a4238b6cf83a7a5079af6f3c8e
farlenkov <farlenkov@gmail.com>
2022-06-23 18:45:21 +0300

    change NetworkMessage.Create to static

--- Runtime/Message/NetworkMessage.cs
+++ Runtime/Message/NetworkMessage.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections;
@@ -10,12 +11,32 @@ namespace UnityNetworkLoop
     {
         public NativeArray<byte> Data;
         public DataStreamWriter Writer;
-        public int Length;
+        public int Length => Writer.Length;
 
-        public void Create(int length)
+        public static NetworkMessage Create(int length, ushort id = 0)
         {
-            Data = new NativeArray<byte>(length, Allocator.Persistent);
-            Writer = new DataStreamWriter(Data);
+            var message = new NetworkMessage();
+            message.Data = new NativeArray<byte>(length, Allocator.Persistent);
+            message.Writer = new DataStreamWriter(message.Data);
+
+            if (id > 0)
+                message.Writer.WriteEntityID(id);
+
+            return message;
+        }
+
+        public static NetworkMessage Create(ushort event_id)
+        {
+            var message = new NetworkMessage();
+            message.Data = new NativeArray<byte>(2, Allocator.Persistent);
+            message.Writer = new DataStreamWriter(message.Data);
+
+            if (event_id > 0)
+                message.Writer.WriteEntityID(event_id);
+            else
+                throw new ArgumentException(string.Format("[NetworkMessage: Create] event_id need to be greater than 0. Current value: {0}", event_id));
+
+            return message;
         }
     }
 }
