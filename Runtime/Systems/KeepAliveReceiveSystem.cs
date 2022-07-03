59523bc5fb76673d24c237cd8937c8e0a9bd5089
farlenkov <farlenkov@gmail.com>
2022-07-03 16:13:52 +0300

    add ReceiveSystem

--- Runtime/Systems/KeepAliveReceiveSystem.cs
+++ Runtime/Systems/KeepAliveReceiveSystem.cs
@@ -6,21 +6,35 @@ using Unity.Networking.Transport;
 
 namespace UnityNetworkLoop
 {
-    public class KeepAliveReceiveSystem : NetworkSystem<NetworkLoop>
+    public class KeepAliveReceiveSystem : ReceiveSystem<NetworkLoop>
     {
-        protected override void Init()
-        {
-            base.Init();
-            Loop.Readers.Add(NetworkMessageType.KeepAlive, ReadKeepAliveMessage);
-        }
+        protected override ushort EventID => NetworkMessageType.KeepAlive;
 
-        void ReadKeepAliveMessage(
+        protected override void ReadMessage(
             ref NetworkConnection connection,
             ref DataStreamReader reader,
-            ushort id)
+            ushort event_id)
         {
             // keep me empty
             // id is the only data we get in this message
         }
     }
+
+    //public class KeepAliveReceiveSystem : NetworkSystem<NetworkLoop>
+    //{
+    //    protected override void Init()
+    //    {
+    //        base.Init();
+    //        Loop.Readers.Add(NetworkMessageType.KeepAlive, ReadKeepAliveMessage);
+    //    }
+
+    //    void ReadKeepAliveMessage(
+    //        ref NetworkConnection connection,
+    //        ref DataStreamReader reader,
+    //        ushort id)
+    //    {
+    //        // keep me empty
+    //        // id is the only data we get in this message
+    //    }
+    //}
 }
---
+++ Runtime/Systems/ReceiveSystem.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Networking.Transport;
+using UnityEngine;
+using UnityGameLoop;
+
+namespace UnityNetworkLoop
+{
+    public abstract class ReceiveSystem<LOOP> : GameLoopManager<LOOP> where LOOP : NetworkLoop
+    {
+        protected virtual ushort EventID => 0;
+        protected virtual ushort StartID => 0;
+        protected virtual ushort EndID => 0;
+
+        protected override void Init()
+        {
+            if (EventID > 0)
+                Loop.Readers.Add(EventID, ReadMessage);
+            else
+                Loop.Readers.Add(StartID, EndID, ReadMessage);
+        }
+
+        protected abstract void ReadMessage(
+            ref NetworkConnection connection,
+            ref DataStreamReader reader,
+            ushort entity_id);
+    }
+}
