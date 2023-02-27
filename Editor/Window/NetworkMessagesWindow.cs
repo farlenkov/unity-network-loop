233dbaae2abbc0b5dd21a10c18fe2ff027f995ae
Mike <work@farlenkov.com>
2023-02-27 18:09:00 +0300

    show max message size in NetworkMessagesWindow

--- Editor/Window/NetworkMessagesWindow.cs
+++ Editor/Window/NetworkMessagesWindow.cs
@@ -4,14 +4,23 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityGameLoop;
+using UnityUtility;
 
 namespace UnityNetworkLoop
 {
     public class NetworkMessagesWindow : EditorWindow
     {
-        Dictionary<ushort, int> ReliableCounters;
-        Dictionary<ushort, int> UnreliableCounters;
+        Dictionary<ushort, Counter> ReliableCounters;
+        Dictionary<ushort, Counter> UnreliableCounters;
         Dictionary<ushort, int> TempCounters;
+        Dictionary<ushort, int> TempSizes;
+
+        public class Counter
+        {
+            public int Count;
+            public int Size;
+        }
+
         bool IsUpdated;
 
         [MenuItem("Window/Network Messages")]
@@ -44,15 +53,13 @@ namespace UnityNetworkLoop
 
         void UpdateCounters(
             NetworkMessageList messages,
-            ref Dictionary<ushort, int> max_counters)
+            ref Dictionary<ushort, Counter> max_counters)
         {
             if (max_counters == null)
-                max_counters = new Dictionary<ushort, int>();
+                max_counters = new Dictionary<ushort, Counter>();
 
-            if (TempCounters == null)
-                TempCounters = new Dictionary<ushort, int>();
-            else
-                TempCounters.Clear();
+            DictionaryExt.Reset(ref TempCounters);
+            DictionaryExt.Reset(ref TempSizes);
 
             for (var i = 0; i < messages.Count; i++)
             {
@@ -62,15 +69,36 @@ namespace UnityNetworkLoop
                     TempCounters.Add(message.EventID, 1);
                 else
                     TempCounters[message.EventID] = count + 1;
+
+                if (!TempSizes.TryGetValue(message.EventID, out var size))
+                    TempSizes.Add(message.EventID, message.Length);
+                else
+                    if (size < message.Length)
+                    TempSizes[message.EventID] = message.Length;
             }
 
             foreach (var current_counter in TempCounters)
             {
-                if (!max_counters.TryGetValue(current_counter.Key, out var max_count))
-                    max_counters.Add(current_counter.Key, current_counter.Value);
+                var messageId = current_counter.Key;
+                var messageCount = current_counter.Value;
+
+                if (!max_counters.TryGetValue(messageId, out var max_counter))
+                    max_counters.Add(messageId, new Counter() { Count = messageCount });
                 else
-                    if (max_count < current_counter.Value)
-                        max_counters[current_counter.Key] = current_counter.Value;
+                    if (max_counter.Count < messageCount)
+                    max_counter.Count = messageCount;
+            }
+
+            foreach (var current_counter in TempSizes)
+            {
+                var messageId = current_counter.Key;
+                var messageSize = current_counter.Value;
+
+                if (!max_counters.TryGetValue(messageId, out var max_counter))
+                    max_counters.Add(messageId, new Counter() { Size = messageSize });
+                else
+                    if (max_counter.Size < messageSize)
+                    max_counter.Size = messageSize;
             }
         }
 
@@ -85,12 +113,18 @@ namespace UnityNetworkLoop
             Draw("Reliable", ReliableCounters);
             Draw("Unreliable", UnreliableCounters);
 
+            if (GUILayout.Button("Reset"))
+            {
+                ReliableCounters.Clear();
+                UnreliableCounters.Clear();
+            }
+
             GUILayout.EndHorizontal();
         }
 
         void Draw(
             string title,
-            Dictionary<ushort, int> counters)
+            Dictionary<ushort, Counter> counters)
         {
             if (counters == null)
                 return;
@@ -101,8 +135,8 @@ namespace UnityNetworkLoop
             foreach (var counter in counters)
             {
                 GUILayout.BeginHorizontal();
-                GUILayout.Label($"{counter.Key}", GUILayout.Width(40));
-                GUILayout.Label($"{counter.Value}", GUILayout.Width(40));
+                GUILayout.Label($"{counter.Key}", GUILayout.Width(26));
+                GUILayout.Label($"{counter.Value.Count} ({counter.Value.Size})", GUILayout.Width(60));
                 GUILayout.EndHorizontal();
             }
 
